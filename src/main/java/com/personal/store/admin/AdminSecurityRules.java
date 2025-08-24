606396c417095e53a8a963054bb935b5d2457860
Yamone Oo <yamone.oo2@frontiir.net>
2025-08-24 16:11:17 +0800

    Refactor the security rules by features

---
+++ src/main/java/com/personal/store/admin/AdminSecurityRules.java
@@ -0,0 +1,15 @@
+package com.personal.store.admin;
+
+import com.personal.store.common.SecurityRules;
+import com.personal.store.users.Role;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AdminSecurityRules implements SecurityRules {
+    @Override
+    public void configure(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry) {
+        registry.requestMatchers("/admin/**").hasRole(Role.ADMIN.name());
+    }
+}
---
+++ src/main/java/com/personal/store/carts/CartSecurityRules.java
@@ -0,0 +1,15 @@
+package com.personal.store.carts;
+
+import com.personal.store.common.SecurityRules;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CartSecurityRules implements SecurityRules {
+
+    @Override
+    public void configure(AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry registry) {
+        registry.requestMatchers("/carts/**").permitAll();
+    }
+}
